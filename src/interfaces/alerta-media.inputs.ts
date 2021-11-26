5d907056d21a743c7a84f090ac6c897d4ca0edc9
Fernando Cortese <cortesefernando@gmail.com>
2021-11-26 18:05:56 -0300

    update modelo alerta media

--- src/interfaces/alerta-media.inputs.ts
+++ src/interfaces/alerta-media.inputs.ts
@@ -1,6 +1,12 @@
-export interface ICrearAlertaMediaInput {
+export interface ICrearAlertaMedia {
   idAlerta: string;
   tipo: string;
   mime: string;
   url: string;
+}
+
+export interface INuevaAlertaMedia {
+  idAlerta: string;
+  tipo: string;
+  mime: string;
 }
