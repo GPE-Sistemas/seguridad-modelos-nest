c25e6bce0b0e54515683b9861daca7e18536a622
Fernando Cortese <cortesefernando@gmail.com>
2021-12-29 19:15:34 -0300

    populate firmware cliente

--- src/interfaces/sirenas/firmware-cliente.model.ts
+++ src/interfaces/sirenas/firmware-cliente.model.ts
@@ -1,6 +1,11 @@
+import { ICliente } from "../cliente.model";
+
 export interface IFirmwareCliente {
   _id: string;
   version: string;
   idCliente: string;
   fecha: string;
+
+  //
+  cliente?: ICliente;
 }
