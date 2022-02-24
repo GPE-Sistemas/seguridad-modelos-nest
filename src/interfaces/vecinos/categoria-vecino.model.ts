088cc1b8c8468bc69c39827336f2d940fe1fd85d
Fernando Cortese <cortesefernando@gmail.com>
2022-02-24 18:26:25 -0300

    id?

--- src/interfaces/vecinos/categoria-vecino.model.ts
+++ src/interfaces/vecinos/categoria-vecino.model.ts
@@ -3,7 +3,7 @@ import { IUsuario } from "../usuario.model";
 import { IVecino } from "./vecino.model";
 
 export interface ICategoriaVecino {
-  _id: string;
+  _id?: string;
   desde: string;
   hasta?: string;
   idCategoria: string;
