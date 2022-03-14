ad06b10daefe7e45914d5a332ba66b1d45abd3ad
Fernando Cortese <cortesefernando@gmail.com>
2022-03-14 15:32:01 -0300

    populate

--- src/interfaces/sirenas/log-sirena.model.ts
+++ src/interfaces/sirenas/log-sirena.model.ts
@@ -1,7 +1,11 @@
+import { ISirena } from "./sirena.model";
+
 export interface ILogSirena {
   _id: string;
   chipId: string;
   fecha: string;
   mensaje?: string;
   origen?: string;
+  //
+  sirena?: ISirena;
 }
