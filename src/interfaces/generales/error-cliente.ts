6916d759bf30e108e7add2814bf569c642210142
Fernando Cortese <cortesefernando@gmail.com>
2022-03-15 17:38:45 -0300

    update modelo error cliente

--- src/interfaces/generales/error-cliente.ts
+++ src/interfaces/generales/error-cliente.ts
@@ -2,10 +2,14 @@ export interface IErrorCliente {
   fecha?: string;
   error: string;
   userAgent?: string;
+  app?: string;
+  versionApp?: string;
 }
 
 export interface ICrearErrorCliente {
   fecha?: string;
   error: string;
   userAgent?: string;
+  app?: string;
+  versionApp?: string;
 }
