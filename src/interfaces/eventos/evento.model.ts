2097b0367b48b670b55de94becabdb536bcba537
Tatoh <tatukis@gmail.com>
2022-09-14 18:02:19 -0300

    id en evento

--- src/interfaces/eventos/evento.model.ts
+++ src/interfaces/eventos/evento.model.ts
@@ -3,6 +3,7 @@ import { ICoordenadas } from '../generales/coordenadas';
 import { IVecino } from '../vecinos/vecino.model';
 
 export interface IEvento {
+  _id: string;
   idCliente: string;
   fecha: string;
   emisor?: string;
