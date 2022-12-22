67f4630a28557fd1f32fc9d332fb1a5df1ecb8a7
Tatoh <tatukis@gmail.com>
2022-12-22 10:56:38 -0300

    direccion y etc

--- src/interfaces/generales/direccionV2.ts
+++ src/interfaces/generales/direccionV2.ts
@@ -1,8 +1,9 @@
 export interface DireccionV2 {
-  calle: string;
-  numero: string;
-  barrio: string;
-  localidad: string;
-  partido: string;
-  provincia: string;
+  calle?: string;
+  numero?: string;
+  barrio?: string;
+  localidad?: string;
+  partido?: string;
+  provincia?: string;
+  direccion?: string;
 }
