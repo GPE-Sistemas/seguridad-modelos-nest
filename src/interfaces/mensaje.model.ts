f1458aa5f02810fd1380d9783a45728fe2d4345b
Fran <etchartfran1997@gmail.com>
2023-01-25 09:28:04 -0300

    IMensaje

--- src/interfaces/mensaje.model.ts
+++ src/interfaces/mensaje.model.ts
@@ -2,8 +2,12 @@ import { ICliente } from "./cliente.model";
 
 export interface IMensaje {
   _id: string;
-  texto: string;
+  idAlerta: string;
   idCliente: string;
+  fecha: string;
+  mensaje: string;
+  texto: string;
+  remitente: string;
   // Virtuals
   cliente?: ICliente;
 }
