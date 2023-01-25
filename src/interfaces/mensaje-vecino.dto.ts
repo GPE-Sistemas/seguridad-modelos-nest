88513f81f6f5e013ba1575f24e18f43286fb3fb5
Fran <etchartfran1997@gmail.com>
2023-01-25 14:24:17 -0300

    NOV-60-mensaje

---
+++ src/interfaces/mensaje-vecino.dto.ts
@@ -0,0 +1,8 @@
+export interface ICrearMensajeVecino {
+  idAlerta: string;
+  mensaje: string;
+}
+
+export interface IUpdateMensajeVecino {
+  mensaje: string;
+}
--- src/interfaces/mensaje.model.ts
+++ src/interfaces/mensaje.model.ts
@@ -2,12 +2,8 @@ import { ICliente } from "./cliente.model";
 
 export interface IMensaje {
   _id: string;
-  idAlerta: string;
   idCliente: string;
-  fecha: string;
-  mensaje: string;
   texto: string;
-  remitente: string;
   // Virtuals
   cliente?: ICliente;
 }
