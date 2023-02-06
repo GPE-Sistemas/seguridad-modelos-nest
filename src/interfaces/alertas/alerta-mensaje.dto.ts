5dda67de5c6f6717fb33f2c17b61fc8e6db7f3a0
Fran <etchartfran1997@gmail.com>
2023-02-06 09:34:00 -0300

    nombre modelo

---
+++ src/interfaces/alertas/alerta-mensaje.dto.ts
@@ -0,0 +1,8 @@
+export interface ICrearAlertaMensaje {
+  idAlerta?: string;
+  mensaje?: string;
+}
+
+export interface IUpdateAlertaMensaje {
+  mensaje?: string;
+}
--- src/interfaces/mensaje-vecino.dto.ts
+++
@@ -1,8 +0,0 @@
-export interface ICrearMensajeVecino {
-  idAlerta?: string;
-  mensaje?: string;
-}
-
-export interface IUpdateMensajeVecino {
-  mensaje?: string;
-}
