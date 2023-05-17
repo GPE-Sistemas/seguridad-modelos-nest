8af592f8bdbda147ddcb26b835e8c6d7fb78cad9
Ignacio Martín Arano <tatukis@gmail.com>
2023-05-17 10:05:42 -0300

    apikey

---
+++ src/interfaces/apiKey/schema.ts
@@ -0,0 +1,11 @@
+import { Rol } from '../generales/rol';
+
+export interface IApiKey {
+  _id?: string;
+  apiName?: string;
+  fechaCreacion?: string;
+  fechaExpiracion?: string;
+  key?: string;
+  idCliente?: string;
+  roles?: Rol[];
+}
