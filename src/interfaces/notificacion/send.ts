916c0cd7add9de0eb9de2695670a5c11e9209dd8
Fernando Cortese <cortesefernando@gmail.com>
2023-05-31 10:47:15 -0300

    send notificacion

---
+++ src/interfaces/notificacion/send.ts
@@ -0,0 +1,11 @@
+export interface ISendNotificacion {
+  enviarA: "Todos" | "Vecinos" | "Localidades" | "Barrios" | string;
+  idsVecinos?: string;
+  idsLocalidades?: string;
+  idsBarrios?: string;
+
+  //
+  titulo?: string;
+  mensaje?: string;
+  data?: { [key: string]: string };
+}
