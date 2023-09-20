cc41ef0ffa9318b7edadbf34ea28f04cb6b3153a
Fernando Cortese <cortesefernando@gmail.com>
2023-09-20 09:26:41 -0300

    nuevo rol

--- src/interfaces/generales/rol.ts
+++ src/interfaces/generales/rol.ts
@@ -1,14 +1,16 @@
 export type Rol =
-  | 'admin'
-  | 'operador'
-  | 'Crear Controles'
-  | 'veedor'
-  | 'Enviar Notificaciones';
+  | "admin"
+  | "operador"
+  | "Crear Controles"
+  | "veedor"
+  | "Enviar Notificaciones"
+  | "Administrador de sirenas";
 
 export const ROLES: Rol[] = [
-  'admin',
-  'operador',
-  'Crear Controles',
-  'veedor',
-  'Enviar Notificaciones',
+  "admin",
+  "operador",
+  "Crear Controles",
+  "veedor",
+  "Enviar Notificaciones",
+  "Administrador de sirenas",
 ];
