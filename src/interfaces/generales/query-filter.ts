5d7939c096518693cdbff3c885a6321d17e0eab7
Fernando Cortese <cortesefernando@gmail.com>
2023-11-28 09:13:14 -0300

    Refactor query-filter interface

--- src/interfaces/generales/query-filter.ts
+++ src/interfaces/generales/query-filter.ts
@@ -1,27 +1,40 @@
 export interface IQueryParam {
-  // page: es el número de página a devolver de los resultados, comenzando desde cero.
   page?: string | number;
-  // limit: es la cantidad de resultados que debe devolver la consulta, cero para sin límite.
   limit?: string | number;
-  // sort: es el orden en que se devolverán los datos, es el nombre de una propiedad de la entidad, ej. ‘username’ para orden ascendente o ‘-username’ para orden descendente.
   sort?: string;
-  // filter: es un objeto con la misma estructura que recibe MongoDb para hacer las querys.
   filter?: string;
-  // populate: es un String u objeto con la misma estructura que recibe Mongoose.
   populate?: string;
-  // select: es un String que contiene separados por espacio todos los campos de la entidad que se desean obtener en la consulta, si no se proporciona se devuelve el objeto completo.
   select?: string;
-  // Se deja un campo genérico para agregar cualquier otro parámetro no contemplado.
   [key: string]: any;
 }
-
-export interface IFilter {
-  [key: string]: any;
-}
-
 export interface IPopulate {
   path?: string;
   select?: string;
   populate?: IPopulate;
   [key: string]: any;
 }
+
+type mongoOperators = {
+  $regex?: string;
+  $in?: string[];
+  $nin?: string[];
+  $gte?: number | string;
+  $lte?: number | string;
+  $gt?: number | string;
+  $lt?: number | string;
+  $eq?: number | string | boolean | null;
+  $ne?: number | string | boolean | null;
+  $exists?: boolean;
+  [key: string]: any;
+};
+
+type mongoFilter = number | string | boolean | null | mongoOperators;
+
+export type IFilter<T> = {
+  [K in keyof T]: undefined | mongoFilter | IFilter<T>[];
+} & {
+  $or?: IFilter<T>[];
+  $and?: IFilter<T>[];
+  $nor?: IFilter<T>[];
+  $not?: mongoFilter;
+};
