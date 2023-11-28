7c61312e31fa3a2009bb509f8b8bcf8deebda40f
Fernando Cortese <cortesefernando@gmail.com>
2023-11-28 11:04:07 -0300

    Refactor IEstadoReclamo interface in estado-reclamo/schema.ts

--- src/interfaces/estado-reclamo/schema.ts
+++ src/interfaces/estado-reclamo/schema.ts
@@ -1,12 +1,13 @@
 import { EstadoReclamo } from "../reclamos";
 
 export interface IEstadoReclamo {
-  idReclamo: string;
+  _id?: string;
+  idReclamo?: string;
   idCliente?: string;
   idVecino?: string;
   idUsuario?: string;
-  fecha: string;
-  estado: EstadoReclamo;
+  fecha?: string;
+  estado?: EstadoReclamo;
   nota?: string;
   notaInterna?: string;
 }
