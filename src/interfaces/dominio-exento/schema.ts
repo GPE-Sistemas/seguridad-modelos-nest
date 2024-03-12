de05d4a2b89597c74b722d1eb2803e265ed6bcd1
Fernando Cortese <cortesefernando@gmail.com>
2024-03-12 10:01:44 -0300

    Actualizar esquema de dominio exento

--- src/interfaces/dominio-exento/schema.ts
+++ src/interfaces/dominio-exento/schema.ts
@@ -1,30 +1,33 @@
-import { ICoordenadas } from '../../auxiliares';
-import { IZonaEstacionamiento } from '../zona-estacionamiento';
+import { ICoordenadas } from "../../auxiliares";
+import { ICliente } from "../cliente";
+import { IZonaEstacionamiento } from "../zona-estacionamiento";
 
 export interface IDominioExcento {
   _id?: string;
   dominio?: string;
-  categoria?: 'Global' | 'Zona' | 'Dirección';
+  categoria?: "Global" | "Zona" | "Dirección";
+  idCliente?: string;
   //Zona
   idZonaEstacionamiento?: IZonaEstacionamiento;
   // Dirección
   direccion?: string;
   coordeandas?: ICoordenadas;
   geojson?: {
-    type: 'Point';
+    type: "Point";
     coordinates: [number, number];
   };
 
   // Virtuals
+  cliente?: ICliente;
   zonaEstacionamiento?: IZonaEstacionamiento;
 }
 
-type OmitirCreate = '_id' | 'zonaEstacionamiento';
+type OmitirCreate = "_id" | "zonaEstacionamiento" | "cliente";
 
 export interface ICreateDominioExcento
   extends Omit<Partial<IDominioExcento>, OmitirCreate> {}
 
-type OmitirUpdate = '_id' | 'zonaEstacionamiento';
+type OmitirUpdate = "_id" | "zonaEstacionamiento" | "cliente" | "idCliente";
 
 export interface IUpdateDominioExcento
   extends Omit<Partial<IDominioExcento>, OmitirUpdate> {}
