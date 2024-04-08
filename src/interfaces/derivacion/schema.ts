9686e2bc7c7a0fb65c4c9e85e28cf71c3de125d0
Fernando Cortese <cortesefernando@gmail.com>
2024-04-08 16:36:43 -0300

    derivacion

---
+++ src/interfaces/derivacion/schema.ts
@@ -0,0 +1,27 @@
+import { ICliente } from "../cliente";
+import { IConfigVecino } from "../config-vecino";
+import { IUsuario } from "../usuario";
+
+export interface IDerivacion {
+  _id?: string;
+  idCliente?: string;
+  idBoton?: string;
+  idCentroMonitoreo?: string;
+  desde?: string;
+  hasta?: string;
+
+  // Virtuals
+  cliente?: ICliente;
+  boton?: IConfigVecino;
+  centroMonitoreo?: IUsuario;
+}
+
+type OmitirCreate = "_id" | "cliente" | "boton" | "centroMonitoreo";
+
+export interface ICreateDerivacion
+  extends Omit<Partial<IDerivacion>, OmitirCreate> {}
+
+type OmitirUpdate = "_id" | "cliente" | "boton" | "centroMonitoreo";
+
+export interface IUpdateDerivacion
+  extends Omit<Partial<IDerivacion>, OmitirUpdate> {}
