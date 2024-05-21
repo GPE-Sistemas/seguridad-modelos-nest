63426d773380f5a486458b35dd292a2ec79871af
Fernando Cortese <cortesefernando@gmail.com>
2024-05-21 15:39:28 -0300

    feat: Agregar campo idCliente a la interfaz ICambioTel

--- src/interfaces/cambio-tel.ts
+++ src/interfaces/cambio-tel.ts
@@ -1,15 +1,16 @@
 export interface ICambioTel {
   _id?: string;
+  idCliente?: string;
   fecha?: string;
   idConfigVecino?: string;
   telefono?: string;
   codigo?: string;
 }
 
-type OmitirCreate = "_id" | "vecinos" | "visitantes";
+type OmitirCreate = "_id";
 export interface ICreateCambioTel
   extends Omit<Partial<ICambioTel>, OmitirCreate> {}
 
-type OmitirUpdate = "_id" | "vecinos" | "visitantes";
+type OmitirUpdate = "_id";
 export interface IUpdateCambioTel
   extends Omit<Partial<ICambioTel>, OmitirUpdate> {}
