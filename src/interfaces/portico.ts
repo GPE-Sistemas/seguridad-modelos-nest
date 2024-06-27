fed582b76617db9410cdf6ef3448b5e9302e48cb
Waranicaton <113130280+Waranicaton@users.noreply.github.com>
2024-06-27 12:48:20 -0300

    agrego config portico

--- src/interfaces/portico.ts
+++ src/interfaces/portico.ts
@@ -1,6 +1,17 @@
 import { ICoordenadas } from "../auxiliares";
 import { ICliente } from "./cliente";
 
+export interface IConfigPortico {
+  type?: string;
+  imgFondo?: string;
+  disSupP?: number;
+  disIzqP?: number;
+  tamP?: number;
+  disSupL?: number;
+  disIzqL?: number;
+  tamL?: number;
+  tamFondo?: number;
+}
 
 export interface IPortico {
   _id?: string;
@@ -14,6 +25,7 @@ export interface IPortico {
   macAddress?: string;
   marcaCamara?: string;
   modeloCamara?: string;
+  configuracion?: IConfigPortico;
   //Populate
   cliente?: ICliente;
 }
@@ -23,8 +35,3 @@ export interface ICreatePortico extends Omit<Partial<IPortico>, OmitirCreate> {}
 
 type OmitirUpdate = "_id" | "cliente";
 export interface IUpdatePortico extends Omit<Partial<IPortico>, OmitirUpdate> {}
-
-
-
-
-
