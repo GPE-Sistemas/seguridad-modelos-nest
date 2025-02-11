e16ef0857b59ca7655d8eab79c62fd97a3721934
Tatoh <tatukis@gmail.com>
2025-02-11 15:27:24 -0300

    refactor: standardize string quotes in boton.ts interface

--- src/interfaces/boton.ts
+++ src/interfaces/boton.ts
@@ -1,4 +1,4 @@
-import { TipoDePunto } from "./puntos";
+import { TipoDePunto } from './puntos';
 
 export interface ICrearTrackeoInput {
   tiempoMinutos?: number;
@@ -33,33 +33,33 @@ export interface IEnvioMultimedia {
 }
 
 export type FuncionBoton =
-  | "Alerta"
-  | "Evento"
-  | "Sirena"
-  | "Reflector"
-  | "911"
-  | "Link"
-  | "Alerta por Punto";
+  | 'Alerta'
+  | 'Evento'
+  | 'Sirena'
+  | 'Reflector'
+  | '911'
+  | 'Link'
+  | 'Alerta por Punto';
 
 export type tipoDato =
-  | "Texto"
-  | "Texto Largo"
-  | "Numero"
-  | "Fecha"
-  | "Booleano"
-  | "Ubicación"
-  | "Foto"
-  | "Persona"
-  | "Lista de personas"
-  | "Texto (Requerido)"
-  | "Texto Largo (Requerido)"
-  | "Numero (Requerido)"
-  | "Fecha (Requerido)"
-  | "Booleano (Requerido)"
-  | "Ubicación (Requerido)"
-  | "Foto (Requerido)"
-  | "Persona (Requerido)"
-  | "Lista de personas (Requerido)";
+  | 'Texto'
+  | 'Texto Largo'
+  | 'Numero'
+  | 'Fecha'
+  | 'Booleano'
+  | 'Ubicación'
+  | 'Foto'
+  | 'Persona'
+  | 'Lista de personas'
+  | 'Texto (Requerido)'
+  | 'Texto Largo (Requerido)'
+  | 'Numero (Requerido)'
+  | 'Fecha (Requerido)'
+  | 'Booleano (Requerido)'
+  | 'Ubicación (Requerido)'
+  | 'Foto (Requerido)'
+  | 'Persona (Requerido)'
+  | 'Lista de personas (Requerido)';
 
 export interface IBoton {
   _id?: string;
@@ -78,6 +78,8 @@ export interface IBoton {
   link?: string;
   // True puede mandar alertas fuera de su zona.
   global?: boolean;
+  // Sirve para los eventos externos
+  prioridad?: number;
   codigoSmartCity?: string;
   // Para elegir uno de tres sonidos
   notificacion?: string;
@@ -97,8 +99,8 @@ export interface IBoton {
   formulario?: { [label: string]: tipoDato };
 }
 
-type OmitirCreate = "_id";
+type OmitirCreate = '_id';
 export interface ICreateBoton extends Omit<Partial<IBoton>, OmitirCreate> {}
 
-type OmitirUpdate = "_id";
+type OmitirUpdate = '_id';
 export interface IUpdateBoton extends Omit<Partial<IBoton>, OmitirUpdate> {}
