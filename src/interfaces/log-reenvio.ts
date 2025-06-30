a4262ae8a1a1262075a0c19dea66f3a0d7bed9be
Tatoh <tatukis@gmail.com>
2025-06-30 09:46:03 -0300

    feat: add Credenciales interface to log-reenvio for enhanced credential management

--- src/interfaces/log-reenvio.ts
+++ src/interfaces/log-reenvio.ts
@@ -1,11 +1,18 @@
 import { ICliente } from './cliente';
 
+export interface Credenciales {
+  usuario: string; // Usuario de las credenciales
+  password: string; // Password de las credenciales
+  apikey?: string; // Apikey de las credenciales
+}
+
 export interface ILogReenvio {
   _id?: string;
   fechaCreacion?: string; // Está al pedo (Sort con _id hace lo mismo, pero es más fácil buscar por fecha con esta propiedad)
   idCliente?: string;
   fechaEnvio?: string; // Fecha del envío
   urlDestino?: string; // URL del destino
+  credenciales?: Credenciales; // Credenciales de la API, si es que se usaron
   tipoEnvio?: 'GET' | 'POST' | 'PUT' | 'DELETE';
   codigoRespuesta?: number; // Código de respuesta HTTP
   respuesta?: Record<string, any>; // Seguro un json o un string
